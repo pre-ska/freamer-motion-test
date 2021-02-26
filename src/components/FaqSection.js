2ebdc10edee3f10354ee4092b57762d24fc17f20
preska <wolfwar@outlook.com>
2021-02-26 23:42:16 +0100

    faq section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,45 @@
+const FaqSection = () => {
+  return (
+    <div>
+      <div className="faq">
+        <h2>
+          Any Questions <span>FAQ</span>
+        </h2>
+        <div className="question">
+          <h4>How do I start?</h4>
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur,
+            omnis!
+          </p>
+        </div>
+        <div className="question">
+          <h4>Daily Schedule</h4>
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur,
+            omnis!
+          </p>
+        </div>
+        <div className="question">
+          <h4>Different Payment methods</h4>
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur,
+            omnis!
+          </p>
+        </div>
+        <div className="question">
+          <h4>What products do you offer?</h4>
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur,
+            omnis!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FaqSection;
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,7 +1,15 @@
 import AboutSection from "../components/AboutSection";
+import FaqSection from "../components/FaqSection";
+import ServicesSection from "../components/ServicesSection";
 
 const AboutUs = () => {
-  return <AboutSection />;
+  return (
+    <>
+      <AboutSection />
+      <ServicesSection />
+      <FaqSection />
+    </>
+  );
 };
 
 export default AboutUs;
