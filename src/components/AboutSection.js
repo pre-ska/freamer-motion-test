addcae8b03ded052baf48f0625871156eca4bf43
preska <wolfwar@outlook.com>
2021-02-27 23:47:41 +0100

    nav created

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,33 +1,35 @@
 import React from "react";
 import home1 from "../img/home1.png";
+import styled from "styled-components";
+import { About, Hide, Description, Image } from "../styles";
 
 const AboutSection = () => {
   return (
-    <div>
-      <div className="description">
+    <About>
+      <Description>
         <div className="title">
           <div className="hide">
             <h2>We work to make</h2>
           </div>
-          <div className="hide">
+          <Hide>
             <h2>
               your <span>dreams</span> come
             </h2>
-          </div>
-          <div className="hide">
+          </Hide>
+          <Hide>
             <h2>true.</h2>
-          </div>
+          </Hide>
         </div>
         <p>
           Contact us for any photography or videography ideas that you have. We
           have professionals with amazing skills.
         </p>
         <button>Contact us</button>
-      </div>
-      <div className="image">
+      </Description>
+      <Image>
         <img src={home1} alt="camera man" />
-      </div>
-    </div>
+      </Image>
+    </About>
   );
 };
 
--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,45 +1,91 @@
+import { About, Hide, Description, Image } from "../styles";
+import styled from "styled-components";
+
 const FaqSection = () => {
   return (
-    <div>
-      <div className="faq">
-        <h2>
-          Any Questions <span>FAQ</span>
-        </h2>
-        <div className="question">
-          <h4>How do I start?</h4>
+    <Faq>
+      <h2>
+        Any Questions <span>FAQ</span>
+      </h2>
+      <div className="question">
+        <h4>How do I start?</h4>
+        <div className="answer">
           <p>Lorem ipsum dolor sit amet.</p>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur,
             omnis!
           </p>
         </div>
-        <div className="question">
-          <h4>Daily Schedule</h4>
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur,
-            omnis!
-          </p>
-        </div>
-        <div className="question">
-          <h4>Different Payment methods</h4>
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur,
-            omnis!
-          </p>
-        </div>
-        <div className="question">
-          <h4>What products do you offer?</h4>
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur,
-            omnis!
-          </p>
-        </div>
+        <div className="faq-line"></div>
       </div>
-    </div>
+      <div className="question">
+        <h4>Daily Schedule</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur,
+            omnis!
+          </p>
+        </div>
+        <div className="faq-line"></div>
+      </div>
+      <div className="question">
+        <h4>Different Payment methods</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur,
+            omnis!
+          </p>
+        </div>
+        <div className="faq-line"></div>
+      </div>
+      <div className="question">
+        <h4>What products do you offer?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur,
+            omnis!
+          </p>
+        </div>
+        <div className="faq-line"></div>
+      </div>
+    </Faq>
   );
 };
+
+const Faq = styled(About)`
+  display: block;
+
+  span: {
+    display: block;
+  }
+
+  h2 {
+    padding-bottom: 2rem;
+    font-weight: lighter;
+  }
+
+  .faq-line {
+    background: #cccccc;
+    height: 0.2rem;
+    margin: 2rem 0rem;
+    width: 100%;
+  }
+
+  .question {
+    padding: 3rem 0rem;
+    cursor: pointer;
+  }
+
+  .answer {
+    padding: 2rem 0rem;
+
+    p {
+      padding: 1rem 0rem;
+    }
+  }
+`;
 
 export default FaqSection;
