092b72f9d056fd2c855a81336501fb35028353d4
preska <wolfwar@outlook.com>
2021-02-28 16:07:05 +0100

    individual movie pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import GlobalStyle from "./components/GlobalStyle";
 import Nav from "./components/Nav";
 import AboutUs from "./pages/AboutUs";
+import { Switch, Router, Route } from "react-router-dom";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
+import MovieDetail from "./pages/MovieDetail";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+
+      <Switch>
+        <Route exact path="/">
+          <AboutUs />
+        </Route>
+        <Route exact path="/work">
+          <OurWork />
+        </Route>
+        <Route exact path="/work/:id">
+          <MovieDetail />
+        </Route>
+        <Route exact path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
